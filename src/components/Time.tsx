17c4e4a882fc0ac6224f27904d48aec12608be1e
sob3r69 <iamnotsob3r@gmail.com>
2023-07-06 14:33:44 +0300

    Added custom titlebar

---
+++ src/components/Time.tsx
@@ -0,0 +1,4 @@
+const Time = () => {
+  return <h1>Time</h1>;
+};
+export default Time;
---
+++ src/components/TitleBar.tsx
@@ -0,0 +1,32 @@
+import './TitleBar.css';
+
+import { useRef } from 'react';
+import { appWindow } from '@tauri-apps/api/window';
+
+const TitleBar = () => {
+  const minimizeRef = useRef<HTMLDivElement>(null);
+  return (
+    <div data-tauri-drag-region className="titlebar">
+      <div
+        className="titlebar-button"
+        id="titlebar-minimize"
+        ref={minimizeRef}
+        onClick={() => appWindow.minimize()}
+      >
+        <img src="https://api.iconify.design/mdi:window-minimize.svg" alt="minimize" />
+      </div>
+      <div
+        className="titlebar-button"
+        id="titlebar-maximize"
+        onClick={() => appWindow.toggleMaximize()}
+      >
+        <img src="https://api.iconify.design/mdi:window-maximize.svg" alt="maximize" />
+      </div>
+      <div className="titlebar-button" id="titlebar-close" onClick={() => appWindow.close()}>
+        <img src="https://api.iconify.design/mdi:close.svg" alt="close" />
+      </div>
+    </div>
+  );
+};
+
+export default TitleBar;
