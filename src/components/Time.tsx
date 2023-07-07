43e038107b187d4f3d73a73a94723267efb03502
sob3r69 <iamnotsob3r@gmail.com>
2023-07-07 14:32:24 +0300

    Added basic functionality

--- src/components/Time.tsx
+++ src/components/Time.tsx
@@ -1,4 +1,24 @@
+import './Time.css';
+import { useEffect, useState } from 'react';
+
 const Time = () => {
-  return <h1>Time</h1>;
+  const [date, setDate] = useState(new Date());
+  const currTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone.split('/');
+
+  useEffect(() => {
+    var timer = setInterval(() => setDate(new Date()), 1000);
+
+    return function cleanup() {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <div className="timer">
+      <h1 className="timer-time">{date.toLocaleTimeString()}</h1>
+      <p>{date.toLocaleDateString()}</p>
+      <div className="timer-timezone">{currTimeZone[1]}</div>
+    </div>
+  );
 };
 export default Time;
--- src/components/TitleBar.tsx
+++ src/components/TitleBar.tsx
@@ -7,23 +7,26 @@ const TitleBar = () => {
   const minimizeRef = useRef<HTMLDivElement>(null);
   return (
     <div data-tauri-drag-region className="titlebar">
-      <div
-        className="titlebar-button"
-        id="titlebar-minimize"
-        ref={minimizeRef}
-        onClick={() => appWindow.minimize()}
-      >
-        <img src="https://api.iconify.design/mdi:window-minimize.svg" alt="minimize" />
-      </div>
-      <div
-        className="titlebar-button"
-        id="titlebar-maximize"
-        onClick={() => appWindow.toggleMaximize()}
-      >
-        <img src="https://api.iconify.design/mdi:window-maximize.svg" alt="maximize" />
-      </div>
-      <div className="titlebar-button" id="titlebar-close" onClick={() => appWindow.close()}>
-        <img src="https://api.iconify.design/mdi:close.svg" alt="close" />
+      <div className="titlebar-title">Clockilo</div>
+      <div className="titlebar-buttons">
+        <div
+          className="titlebar-button"
+          id="titlebar-minimize"
+          ref={minimizeRef}
+          onClick={() => appWindow.minimize()}
+        >
+          <img src="https://api.iconify.design/mdi:window-minimize.svg" alt="minimize" />
+        </div>
+        <div
+          className="titlebar-button"
+          id="titlebar-maximize"
+          onClick={() => appWindow.toggleMaximize()}
+        >
+          <img src="https://api.iconify.design/mdi:window-maximize.svg" alt="maximize" />
+        </div>
+        <div className="titlebar-button" id="titlebar-close" onClick={() => appWindow.close()}>
+          <img src="https://api.iconify.design/mdi:close.svg" alt="close" />
+        </div>
       </div>
     </div>
   );
